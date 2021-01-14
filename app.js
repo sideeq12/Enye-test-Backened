1e2713c1229791b310c0d9d60095a1a5fcfa4a9f
sideeq12 <abdwaheed2018@gmail.com>
2021-01-14 01:35:34 -0800

    status

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const https = require("https");
+
+const app = express();
+
+
+//declaring the routes
+
+
+app.get("/", (req, res)=>{
+    
+})
+
+app.listen(3000, ()=>{
+    console.log("server running on port 3000")
+})
